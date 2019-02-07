19a7564641f1080279c0f3f2563beb32e2b28841
alefix <alefix@qq.com>
2019-02-07 10:55:17 +0100

    add some extensions
    barryvdh/laravel-ide-helper
    barryvdh/laravel-debugbar
    doctrine/dbal

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Force HTTPS
+        if (env('REDIRECT_HTTPS')) {
+            URL::forceScheme('https');
+        }
+
+        Schema::defaultStringLength(191);
     }
 
     /**
